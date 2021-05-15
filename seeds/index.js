81076d77b2a886ff0afa85880f19b35b9d1126d6
Kennet Guevara <kennethgue881@gmail.com>
2021-05-15 13:03:24 -0600

    Image upload, remove .env from git and starability implementation. Controller refactoring

--- seeds/index.js
+++ seeds/index.js
@@ -27,9 +27,18 @@ const seedDB = async () => {
             author: '609d667322a725644802d0be',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione ab nostrum tempore perspiciatis, placeat neque veniam modi rerum perferendis vero nulla aspernatur reprehenderit ipsa officia unde corrupti id laudantium possimus!',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dmi3zyqqf/image/upload/v1621014417/YelpCamp/j41iqumjryv0z9iwr1va.png',
+                    filename: 'YelpCamp/j41iqumjryv0z9iwr1va'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dmi3zyqqf/image/upload/v1621014420/YelpCamp/jgriaf028mvdyr1jyop2.png',
+                    filename: 'YelpCamp/jgriaf028mvdyr1jyop2'
+                }
+            ]
         })
         await camp.save();
     }
